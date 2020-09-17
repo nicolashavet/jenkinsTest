b3899ba48a8796d05c305c33f3e257e63c1ae434
nicolashavet <nico.havet2@hotmail.fr>
2020-09-17 16:44:22 +0200

    get configuration from json file

--- parameters.groovy
+++ parameters.groovy
@@ -1,1 +1,7 @@
-return(["alice","bob"])
+def fileContents = readFile 'C:\\jenkinsConfiguration\\test.json'
+def jsonObj = readJSON text: fileContents
+
+return jsonObj.name
+
+
+//  return(["alice","bob"])
